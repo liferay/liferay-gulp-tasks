de87b017fdadae6311f8e477132e541f9d0bdfae
Jonathan Mak <jonathan.mak@liferay.com>
2016-02-22 17:28:47 -0800

    Use spaces

--- index.js
+++ index.js
@@ -13,58 +13,58 @@ module.exports.registerTasks = function(opt_options) {
   var finalName = packageJson.name + '-' + packageJson.version;
 
   gulp.task('prepare-maven-snapshot', function() {
-	   return gulp.src(['**/*', '!node_modules/', '!node_modules/**'])
-  		.pipe(gulp.dest(
+     return gulp.src(['**/*', '!node_modules/', '!node_modules/**'])
+      .pipe(gulp.dest(
         path.join('maven-dist/META-INF/resources/webjars', packageJson.name, packageJson.version + '-SNAPSHOT')
       ));
   });
 
   gulp.task('install-maven-snapshot', function() {
-  	return gulp.src('.')
-  		.pipe(maven.install({
-  			'config': {
+    return gulp.src('.')
+      .pipe(maven.install({
+        'config': {
           'artifactId': packageJson.name,
           'buildDir': 'maven-dist',
           'finalName': finalName,
           'groupId': 'com.liferay.webjars',
           'type': 'jar',
           'version': packageJson.version
-  			}
-  		}));
+        }
+      }));
   });
 
   gulp.task('prepare-maven-artifact', function() {
-  	return gulp.src(['**/*', '!node_modules/', '!node_modules/**'])
-  		.pipe(gulp.dest(
+    return gulp.src(['**/*', '!node_modules/', '!node_modules/**'])
+      .pipe(gulp.dest(
         path.join('maven-dist/META-INF/resources/webjars', packageJson.name, packageJson.version)
       ));
   });
 
   gulp.task('publish-maven-artifact', function() {
-  	return gulp.src('.')
-  		.pipe(maven.deploy({
-  			'config': {
-  				'artifactId': packageJson.name,
-  				'buildDir': 'maven-dist',
-  				'finalName': finalName,
-  				'groupId': 'com.liferay.webjars',
-  				'type': 'jar',
+    return gulp.src('.')
+      .pipe(maven.deploy({
+        'config': {
+          'artifactId': packageJson.name,
+          'buildDir': 'maven-dist',
+          'finalName': finalName,
+          'groupId': 'com.liferay.webjars',
+          'type': 'jar',
           'version': packageJson.version,
-  				'repositories': [
-  					{
-  						'id': 'liferay-nexus-ce',
-  						'url': 'https://repository.liferay.com/nexus/content/repositories/liferay-releases-ce/'
-  					}
-  				]
-  			}
-  		}));
+          'repositories': [
+            {
+              'id': 'liferay-nexus-ce',
+              'url': 'https://repository.liferay.com/nexus/content/repositories/liferay-releases-ce/'
+            }
+          ]
+        }
+      }));
   });
 
   gulp.task('maven-install', function(done) {
-  	runSequence('prepare-maven-snapshot', 'install-maven-snapshot', done);
+    runSequence('prepare-maven-snapshot', 'install-maven-snapshot', done);
   });
 
   gulp.task('maven-publish', function(done) {
-  	runSequence('prepare-maven-artifact', 'publish-maven-artifact', done);
+    runSequence('prepare-maven-artifact', 'publish-maven-artifact', done);
   });
 };
